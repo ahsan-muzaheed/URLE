afb9895021b3a98246f628fc93395c18f9d86865
Abir Islam Anupom <onupomabir@gmail.com>
2023-09-04 01:05:10 +0600

    product_infos

--- script.js
+++ script.js
@@ -1,106 +1,40 @@
-// JavaScript for index.html
-
-// Sample data for products
+// Datas of Products
 const Data = [
-    { 
+    {
         id: 1,
-        imageSrc: 'images/img1.webp', 
+        imageSrc: 'images/img1.webp',
         title: 'Box 1',
         subTitle: 'AD Gaming',
-        link: 'product.html?id=1', // Modify the link to include the product ID
+        link: 'product.html?id=1',
         offer: '50% OFF',
         price: '$12.44',
         category: 'Character',
-    },
-    { 
-        id: 2,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 2',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=2', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 3,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 4,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 5,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 6,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 7,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 8,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 9,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
-    },
-    { 
-        id: 10,
-        imageSrc: 'images/img1.webp', 
-        title: 'Box 3',
-        subTitle: 'AD Gaming',
-        link: 'product.html?id=3', // Modify the link to include the product ID
-        offer: '50% OFF',
-        price: '$12.44',
-        category: 'Character',
+        desc : 'Here you can find a lot of coral models, at all it is around 79 units.',
+
+        issueDate: '1st September 2023',
+        rate: '<i class="fa-solid fa-star"></i>' + '<i class="fa-solid fa-star"></i>' + 
+            '<i class="fa-solid fa-star"></i>' + '<i class="fa-solid fa-star"></i>' + 
+            '<i class="fa-solid fa-star"></i>',
+        numberReview: '23 Reviews Written',
+        questAnswered: '5 of 6 Question Answered',
+
+        sliderImgs: [
+            {
+                slideImg: 'images/Coral1.jpg',
+            },
+            {
+                slideImg: 'images/Coral2.jpg',
+            },
+            {
+                slideImg: 'images/Coral3.avif',
+            },
+            {
+                slideImg: 'images/Coral4.webp',
+            },
+            {
+                slideImg: 'images/Coral5.jpg'
+            },
+        ]
     },
 ];
 
@@ -111,18 +45,16 @@ function generateProductList() {
     Data.forEach(item => {
         const box = document.createElement("a");
         box.href = `product.html?id=${item.id}`;
-        // box.textContent = item.title;
         box.classList.add("box");
 
 
-        // start here 
         const image = document.createElement('img');
         image.src = item.imageSrc;
         image.alt = item.title;
-    
+
         const title = document.createElement('h5');
         title.textContent = item.title;
-    
+
         const subTitle = document.createElement('p');
         subTitle.textContent = item.subTitle;
 
@@ -154,53 +86,89 @@ function generateProductList() {
         box.appendChild(image);
         box.appendChild(boxContent);
         box.appendChild(category)
-        // end here
 
 
         productList.appendChild(box);
     });
 }
-
-// Check if we're on the home page and call the appropriate function
 if (document.querySelector(".boxes")) {
     generateProductList();
 }
 
 
-
-// JavaScript for product.html
-
-// Sample data for products
-const products = [
-    { id: 1, name: "Product 1", description: "Description 1" },
-    { id: 2, name: "Product 2", description: "Description 2" },
-    { id: 3, name: "Product 3", description: "Description 3" },
-    { id: 4, name: "Product 4", description: "Description 4" },
-    // Add more products here
-];
-
-// Function to display product details on the product page
+// data showing for Product page
 function displayProductDetails() {
-    const productDetails = document.getElementById("product-details");
+    const wrapper2 = document.querySelector(".wrapper2");
+    const wrapper1 = document.querySelector(".wrapper1");
     const urlParams = new URLSearchParams(window.location.search);
     const productId = parseInt(urlParams.get("id"));
+
+
+
+
 
     const selectedProduct = Data.find(product => product.id === productId);
 
     if (selectedProduct) {
-        const productTitle = document.createElement("h2");
-        productTitle.textContent = selectedProduct.title;
+        // const productTitle = document.createElement("h2");
+        // productTitle.textContent = selectedProduct.title;
 
-        const productDescription = document.createElement("p");
-        productDescription.textContent = selectedProduct.description;
+        // const productDescription = document.createElement("p");
+        // productDescription.textContent = selectedProduct.offer;
 
-        productDetails.appendChild(productTitle);
-        productDetails.appendChild(productDescription);
+        // productDetails.appendChild(productTitle);
+        // productDetails.appendChild(productDescription);
+
+        selectedProduct.sliderImgs.forEach(sImg => {
+            // for wrapper2
+            const swiperSlide = document.createElement('div');
+            swiperSlide.classList.add('swiper-slide');
+
+            const slideImg = document.createElement('img');
+            slideImg.src = sImg.slideImg;
+
+            swiperSlide.appendChild(slideImg);
+            wrapper2.appendChild(swiperSlide);
+
+
+            // for wrapper2
+            const swiperSlide1 = document.createElement('div');
+            swiperSlide1.classList.add('swiper-slide');
+
+            const slide1Img = document.createElement('img');
+            slide1Img.src = sImg.slideImg;
+
+            swiperSlide1.appendChild(slide1Img);
+            wrapper1.appendChild(swiperSlide1);
+        })
+
+        // for product information in product description page
+        const productInfos = document.querySelector('.product__infos')
+        const productTitle = document.querySelector('.product__title')
+        const issueDate = document.querySelector('.issue__date')
+        const rate = document.querySelector('.rate')
+        const rate__num = document.querySelector('.rate__num')
+        const rate__read = document.querySelector('.rate__read')
+        const prod__desc = document.querySelector('.prod__desc')
+
+
+        productTitle.textContent = selectedProduct.title
+        issueDate.textContent = selectedProduct.issueDate
+        rate.innerHTML = selectedProduct.rate
+        rate__num.textContent = selectedProduct.numberReview
+        rate__read.textContent = selectedProduct.questAnswered
+        prod__desc.textContent = selectedProduct.desc
+
+
+
     } else {
         productDetails.textContent = "Product not found.";
     }
 }
-
-// Call the function to display product details
 displayProductDetails();
+
+
+
+// swiper js code
+
 
